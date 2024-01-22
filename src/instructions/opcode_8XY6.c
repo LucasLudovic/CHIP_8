b2b5e109b4c17fdf5b9858aafc70a80f6a131379
Valiance <lucas.ludovic@epitech.eu>
2024-01-22 16:24:44 +0100

    [UPDATE] Adding instructions for opcode 8XY6

--- src/instructions/opcode_8XY6.c
+++ src/instructions/opcode_8XY6.c
@@ -9,3 +9,12 @@
 #include "my_types.h"
 #include "my.h"
 #include "my_macros.h"
+
+int opcode_8XY6(emulator_t *emulator, UNUSED Uint8 b1, UNUSED Uint8 b2, Uint8 b3)
+{
+    if (emulator == NULL || emulator->cpu == NULL)
+        return display_error("Unable to access CPU with opcode 8XY6\n");
+    emulator->cpu->memory_register[NB_REGISTER - 1] = emulator->cpu->memory_register[b3] & 0x01;
+    emulator->cpu->memory_register[b3] = emulator->cpu->memory_register[b3] >> 1;
+    return SUCCESS;
+}
