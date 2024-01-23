8b63bb9627c560f51290e0af7862d446b3ea7bdd
Valiance <lucas.ludovic@epitech.eu>
2024-01-23 11:39:26 +0100

    [UPDATE] Adding the functions pointer to the table

--- src/instructions/opcode_00EE.c
+++ src/instructions/opcode_00EE.c
@@ -8,7 +8,7 @@
 #include "emulator/emulate.h"
 #include "my_macros.h"
 
-int opcode_OOEE(emulator_t *emulator, UNUSED Uint8 b1, UNUSED Uint8 b2, UNUSED Uint8 b3)
+int opcode_00EE(emulator_t *emulator, UNUSED Uint8 b1, UNUSED Uint8 b2, UNUSED Uint8 b3)
 {
     if (emulator == NULL || emulator->cpu == NULL)
         return FAILURE;
